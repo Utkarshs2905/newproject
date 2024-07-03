df0a990848983863796a770634c574b9b7f4d429
iamshubhgadekar <163977941+iamshubhgadekar@users.noreply.github.com>
2024-07-03 14:33:21 +0530

    hello.java

--- helloword.java
+++ helloword.java
@@ -1,11 +1,8 @@
-// This is a simple Java program.
-// FileName : "HelloWorld.java".
 
-class HelloWorld {
-	// Your program begins with a call to main().
-	// Prints "Hello, World" to the terminal window.
+class Hello {
+	
 	public static void main(String args[])
 	{
-		System.out.println("Hello, World");
+		System.out.println("Hello World");
 	}
 }
